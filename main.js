3736c912ad5ae2532ad5d26ba6ab8bcdd4c83143
Asher Lee-Tyson <aleetyson@gmail.com>
2022-08-30 12:33:05 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+let randomNumber = Math.floor(Math.random() * 10);
+if (randomNumber === 1) {
+    console.log('A single arrow is easily broken; a bundle of ten is not')
+} else if (randomNumber === 2) {
+    console.log('You can/t get better unless you get started')
+} else if (randomNumber === 3) {
+    console.log('The truth always carries the ambiguity of the words used to express it')
+} else if (randomNumber === 4) {
+    console.log('Position = perspective')
+} else if (randomNumber === 5) {
+    console.log('Experience is the name we give to our mistakes - Oscar Wilde')
+} else if (randomNumber === 6) {
+    console.log('Theory vs. practice')
+} else if (randomNumber === 7) {
+    console.log('The shortcut is to stop taking shortcuts')
+} else if (randomNumber === 8) {
+    console.log('Happiness depends upon ourselves - Aristotle')
+} else if (randomNumber === 9) {
+    console.log('If you love nature, you will find beauty everywhere - Vincent van Gogh')
+}
+
+function returnQuote() {
+    
+}
