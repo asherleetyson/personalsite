0a721d95d9c91ca28af0428f8f435760de508ad5
Asher Lee-Tyson <aleetyson@gmail.com>
2022-08-30 16:48:26 -0500

    Second Commit

--- main.js
+++ main.js
@@ -1,24 +1,12 @@
-let randomNumber = Math.floor(Math.random() * 10);
-if (randomNumber === 1) {
-    console.log('A single arrow is easily broken; a bundle of ten is not')
-} else if (randomNumber === 2) {
-    console.log('You can/t get better unless you get started')
-} else if (randomNumber === 3) {
-    console.log('The truth always carries the ambiguity of the words used to express it')
-} else if (randomNumber === 4) {
-    console.log('Position = perspective')
-} else if (randomNumber === 5) {
-    console.log('Experience is the name we give to our mistakes - Oscar Wilde')
-} else if (randomNumber === 6) {
-    console.log('Theory vs. practice')
-} else if (randomNumber === 7) {
-    console.log('The shortcut is to stop taking shortcuts')
-} else if (randomNumber === 8) {
-    console.log('Happiness depends upon ourselves - Aristotle')
-} else if (randomNumber === 9) {
-    console.log('If you love nature, you will find beauty everywhere - Vincent van Gogh')
-}
+var quotes = [
+    '"Let the improvement of yourself keep you so busy that you have no time to criticize others." — Roy T. Bennett',
+    '"Courage is not the absence of fear, but rather the assessment that something else is more important than fear." — Franklin D. Roosevelt',
+    '"Care about what other people think and you will always be their prisoner." — Lao Tzu',
+    '"Whether you think you can, or you think you can\'t, you\'re right." — Henry Ford'
+]
+
 
 function returnQuote() {
-    
+    var randomNumber = Math.floor(Math.random() * (quotes.length));
+    document.getElementById('quote-text').innerHTML = quotes[randomNumber];
 }
